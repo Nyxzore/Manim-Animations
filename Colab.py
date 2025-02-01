1f1f77758ad8ad2abf1084e3e54ad6b1a09dc3f3
Nyxzore <152401939+Nyxzore@users.noreply.github.com>
2025-02-01 11:03:42 +0200

    kabam

--- Colab.py
+++ Colab.py
@@ -7,7 +7,7 @@ class RestoreExample(Scene):
         self.EvenFunction()
         self.Feynman()
     def VisualUnderCurve(self):
-        equation = MathTex(r"f(x) = \frac{sin^2x}{x^2}", font_size = 60)
+        equation = MathTex(r"f(x) = \frac{\sin^2x}{x^2}", font_size = 60)
         self.play(Write(equation))
         self.play(equation.animate.align_on_border(UP))
 
@@ -27,7 +27,7 @@ class RestoreExample(Scene):
 
         area_formula = MathTex(r"F(x) = ").align_on_border(DOWN).shift(LEFT)
         self.play(Write(area_formula))
-        anti_derivitve = MathTex(r"\int_{-\infty}^{\infty}\frac{sin^2x}{x^2}dx", font_size = 60).next_to(area_formula, RIGHT)
+        anti_derivitve = MathTex(r"\int_{-\infty}^{\infty}\frac{\sin^2x}{x^2}dx", font_size = 60).next_to(area_formula, RIGHT)
         self.play(Transform(area, anti_derivitve), run_time = 2)
         self.wait(1)
 
@@ -38,13 +38,13 @@ class RestoreExample(Scene):
     def EvenFunction(self):
         self.play(area.animate.align_on_border(UP))
 
-        fofx = MathTex(r"f(x)= \frac{sin^2x}{x^2}")
-        fofnx = MathTex(r"f(-x)= \frac{sin^2(-x)}{(-x)^2}").next_to(fofx,DOWN)
+        fofx = MathTex(r"f(x)= \frac{\sin^2x}{x^2}")
+        fofnx = MathTex(r"f(-x)= \frac{\sin^2(-x)}{(-x)^2}").next_to(fofx,DOWN)
         self.play(Write(fofx), run_time = 2)
         self.play(Write(fofnx), run_time = 2)
 
         #simplification
-        fofnx2 = MathTex(r"f(-x) = \frac{sin^2x}{x^2}").next_to(fofx,DOWN)
+        fofnx2 = MathTex(r"f(-x) = \frac{\sin^2x}{x^2}").next_to(fofx,DOWN)
         self.play(Transform(fofnx, fofnx2), run_time = 2)
 
         conclusion = MathTex(r"\therefore f(x)=f(-x) \therefore \text{f is even}").next_to(fofnx,DOWN)
@@ -52,12 +52,12 @@ class RestoreExample(Scene):
 
         self.play(FadeOut(fofx, fofnx, conclusion))
         self.play(area.animate.move_to(LEFT))
-        double_int = MathTex(r"= 2\int_{0}^{\infty}\frac{sin^2x}{x^2}dx", font_size = 60).next_to(area, RIGHT)
+        double_int = MathTex(r"= 2\int_{0}^{\infty}\frac{\sin^2x}{x^2}dx", font_size = 60).next_to(area, RIGHT)
         self.play(Write(double_int), run_time = 2)
 
         #I definition
         global let
-        let = MathTex(r"\text{let } I = \int_{0}^{\infty}\frac{sin^2x}{x^2}dx").align_on_border(UP)
+        let = MathTex(r"\text{let } I = \int_{0}^{\infty}\frac{\sin^2x}{x^2}dx").align_on_border(UP)
         self.play(Write(let))
         self.play(Indicate(let[0][5:], color=BLUE), Indicate(double_int[0][2:], color=BLUE), run_time = 1)
 
@@ -65,9 +65,132 @@ class RestoreExample(Scene):
         self.play(FadeOut(double_int, area))
         
     def Feynman(self):
-        Parameter = MathTex(r"I(\alpha) = \int_{0}^{\infty}\frac{sin^2(\alpha x)}{x^2}dx").next_to(let,DOWN)
+        Parameter = MathTex(r"I(\alpha) = \int_{0}^{\infty}\frac{\sin^2(\alpha x)}{x^2}dx").next_to(let,DOWN)
         self.play(Write(Parameter), run_time = 3)
         self.wait(2)
-        IPrime = MathTex(r"I'(\alpha) = \frac{d}{d\alpha}\int_{0}^{\infty}\frac{sin^2(\alpha x)}{x^2}dx").next_to(Parameter,DOWN)
+        IPrime = MathTex(r"I'(\alpha) = \frac{d}{d\alpha}\int_{0}^{\infty}\frac{\sin^2(\alpha x)}{x^2}dx").next_to(Parameter,DOWN)
         self.play(Write(IPrime), run_time = 3)
         self.wait(1)        
+
+
+        int_sign = VGroup(IPrime[0][10],IPrime[0][11],IPrime[0][12])
+        ddx = VGroup(IPrime[0][6], IPrime[0][7], IPrime[0][8], IPrime[0][9])
+        self.play(int_sign.animate.move_to(ddx), ddx.animate.move_to(int_sign))
+
+        self.play(Indicate(IPrime[0][6], color=RED), Indicate(IPrime[0][8], color=RED))
+        self.play(  Transform(IPrime[0][6], MathTex(r"\partial").move_to(IPrime[0][6])), 
+                    Transform(IPrime[0][8], MathTex(r"\partial").move_to(IPrime[0][8])))
+        
+        self.play(Indicate(IPrime[0][9], color=BLUE), Indicate(IPrime[0][18], color=BLUE))
+
+        IPrime2 = MathTex(r"I'(\alpha) = \int_{0}^{\infty} \frac{1}{x^2} \frac{\partial}{\partial \alpha} \sin^2(\alpha x)} \, dx").next_to(IPrime,DOWN)
+        self.play(Write(IPrime2), run_time = 2)
+
+        #ddxsinsquared
+        ddxsquared = MathTex(r"\frac{d}{dx}sin^2(ax) = 2sin(ax)cos(ax).x").next_to(IPrime2, DOWN)
+
+
+        ddxbox = SurroundingRectangle(ddxsquared, color=BLUE)
+        self.play(Create(ddxbox))
+        self.play(Write(ddxsquared), runtime = 2)
+        self.wait(1)
+        self.play(FadeOut(ddxsquared, ddxbox))
+
+        IPrime3 = MathTex(r"I'(\alpha) = \int_{0}^{\infty} \frac{1}{x^2} 2\sin(\alpha x)\cos(\alpha x).x \, dx").next_to(IPrime2,DOWN)
+        self.play(Write(IPrime3), run_time = 3)
+
+        cancel_x_one = Line(start=IPrime3[0][-3].get_corner(UL), end=IPrime3[0][-3].get_corner(DR), color=BLUE)
+        cancel_x_two = Line(start=IPrime3[0][12].get_corner(UL), end=IPrime3[0][12].get_corner(DR), color=BLUE)
+
+        self.play(Create(cancel_x_one), Create(cancel_x_two))
+
+        self.play(FadeOut(IPrime, IPrime2, cancel_x_one, cancel_x_two))
+        self.play(IPrime3.animate.move_to(IPrime))
+
+        IPrime4 = MathTex(r"I'(\alpha) = \int_{0}^{\infty} \frac{2\sin(\alpha x)\cos(\alpha x)}{x}\, dx").next_to(Parameter,DOWN)
+        self.play(Transform(IPrime3, IPrime4))
+
+        double_angle_identity = MathTex(r"\sin (2x) = 2\sin x \cos x").next_to(IPrime3, DOWN)
+        double_angle_identity_box = SurroundingRectangle(double_angle_identity, color=BLUE)
+
+        self.play(Create(double_angle_identity_box))
+        self.play(Write(double_angle_identity), run_time = 2)
+        
+        numerator = VGroup()
+        for i in range(11,28,1):
+            numerator = VGroup(numerator, IPrime3[0][i])
+        self.play(Indicate(double_angle_identity[0][8:], color=BLUE), Indicate(numerator, color=BLUE))
+
+        self.play(Transform(numerator, MathTex(r"\sin (2 \alpha x)").move_to(numerator)))
+
+        ################## J
+        self.play(FadeOut(double_angle_identity, double_angle_identity_box, let, Parameter))
+        IPrime3 = VGroup(IPrime3, numerator)
+        self.play(IPrime3.animate.align_on_border(UP))
+
+        JParameter = MathTex(r"I'(\beta)= \int_{0}^{\infty} \frac{\sin (2 \alpha x)}{x} e^{-\beta x} \, dx").next_to(IPrime3, DOWN)
+
+        self.play(Write(JParameter), run_time = 3)
+        self.wait(1)
+
+        
+        self.play(FadeOut(IPrime3))
+        self.play(JParameter.animate.align_on_border(UP))
+
+        self.play(Indicate(JParameter[0][7], color=BLUE), Indicate(JParameter[0][-5], color=BLUE), run_time = 2)
+        jprime = MathTex(r"I''(\beta)= \frac{d}{d \beta} \int_{0}^{\infty} \frac{\sin (2 \alpha x)}{x} e^{-\beta x} \, dx").next_to(JParameter, DOWN)
+        self.play(Write(jprime), run_time = 3)
+
+        int_sign = VGroup(jprime[0][11],jprime[0][12],jprime[0][13])
+        ddx = VGroup(jprime[0][7], jprime[0][8], jprime[0][9], jprime[0][10])
+        self.play(int_sign.animate.move_to(ddx), ddx.animate.move_to(int_sign))
+
+        self.play(Indicate(jprime[0][7], color=RED), Indicate(jprime[0][9], color=RED))
+        self.play(  Transform(jprime[0][7], MathTex(r"\partial").move_to(jprime[0][7])), 
+                    Transform(jprime[0][9], MathTex(r"\partial").move_to(jprime[0][9])))
+        
+        self.play(Indicate(jprime[0][9], color=BLUE), Indicate(jprime[0][-4], color=BLUE))
+
+        ddbe = MathTex(r"\frac{\partial}{\partial \beta} e^{-\beta x} = -xe^{-xb}")
+        ddxbox = SurroundingRectangle(ddbe, color=BLUE)
+        self.play(Create(ddxbox))
+        self.play(Write(ddbe), runtime = 2)
+        self.wait(1)
+        self.play(FadeOut(ddbe, ddxbox))
+
+        jprime2 = MathTex(r"I''(\beta)= \int_{0}^{\infty} \frac{\sin (2 \alpha x)}{x} (-x) \cdot e^{-\beta x} \, dx").next_to(jprime, DOWN)
+
+        self.play(Write(jprime2, run_time = 2))
+        cancel_x_one = Line(start=jprime2[0][26].get_corner(UL), end=jprime2[0][26].get_corner(DR), color=BLUE)
+        cancel_x_two = Line(start=jprime2[0][23].get_corner(UL), end=jprime2[0][23].get_corner(DR), color=BLUE)
+
+        self.play(Create(cancel_x_one), Create(cancel_x_two))
+
+        jprime3 = MathTex(r"I''(\beta)= -\int_{0}^{\infty} \sin (2 \alpha x) \cdot e^{-\beta x} \, dx").next_to(jprime, DOWN)
+        self.play(FadeOut(cancel_x_one, cancel_x_two))
+        self.play(Transform(jprime2, jprime3))
+
+        self.wait(1)
+
+
+        self.play(FadeOut(jprime, JParameter))
+        self.play(jprime2.animate.align_on_border(UP))
+
+        ibp = MathTex(r"\int f(x) g'(x) \, dx = f(x) g(x) - \int f'(x) g(x) \, dx").next_to(jprime2, DOWN)
+        ibpnew = MathTex(r"- \int f(x) g'(x) \, dx = -f(x) g(x) + \int f'(x) g(x) \, dx").next_to(jprime2, DOWN)
+        IBPBox = SurroundingRectangle(ibpnew, color=BLUE)
+        self.play(Create(IBPBox))
+        self.play(Write(ibp), runtime = 2)
+        self.wait(1)
+        
+        self.play(Transform(ibp, ibpnew), runtime = 2)
+
+        self.play(FadeOut(ibp, IBPBox))
+
+        ibp1 = MathTex(
+            r"-\int_{0}^{\infty} \sin (2 \alpha x) \cdot e^{-\beta x} \, dx = ",
+            r"-\left[\frac{(- \cos(2\alpha x))}{2\alpha}e^{-\beta x}\right]_0^{\infty} ",
+            r"+ \int_{0}^{\infty} \frac{-\cos(2\alpha x)}{2\alpha}(-\beta e^{-\beta x}) \, dx"
+            ).next_to(jprime2, DOWN).scale(0.7)
+
+        self.play(Write(ibp1), run_time = 6)
